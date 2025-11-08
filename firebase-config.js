d8d70e85ead0d960c331f61c250240f297c1e6ee
Thalaivar <t96halaivar@gmail.com>
2025-11-09 02:19:30 +0530

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDCiwSBQtXSDGQEUa08UCSBwi_o_ls7Af0",
+    authDomain: "vantablackx-0.firebaseapp.com",
+    databaseURL: "https://vantablackx-0-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "vantablackx-0",
+    storageBucket: "vantablackx-0.appspot.com",
+    messagingSenderId: "155034966083",
+    appId: "1:155034966083:web:499bb8aaac1d815500dcf6",
+    measurementId: "G-MREYTXQDVJ"
+  };
